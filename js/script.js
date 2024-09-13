c28aa34c4d46085f2f35d242ca19dd8f772c123a
LUCAS PONZONI <139734105+Lucasponzoni@users.noreply.github.com>
2024-09-13 09:39:44 -0300

    Actualizacion

--- js/script.js
+++ js/script.js
@@ -16,111 +16,287 @@ function rellenarMedidas() {
 
     switch (tipoElectrodomestico) {
         case "heladera":
-            alto = 165; ancho = 60; largo = 60; peso = 60; valor = 700000;
+            alto = 165; 
+            ancho = 60; 
+            largo = 60; 
+            peso = 60; 
+            valor = 700000;
             break;
         case "cocina":
-            alto = 85; ancho = 60; largo = 60; peso = 50; valor = 600000;
+            alto = 85; 
+            ancho = 60; 
+            largo = 60; 
+            peso = 50; 
+            valor = 600000;
             break;
         case "hornoEmpotrable":
-            alto = 60; ancho = 60; largo = 55; peso = 25; valor = 500000;
+            alto = 60; 
+            ancho = 60; 
+            largo = 55; 
+            peso = 25; 
+            valor = 500000;
             break;
         case "lavavajillas":
-            alto = 85; ancho = 60; largo = 60; peso = 45; valor = 600000;
+            alto = 85; 
+            ancho = 60; 
+            largo = 60; 
+            peso = 45; 
+            valor = 600000;
             break;
+        case "aireportatil":
+            alto = 75; 
+            ancho = 40; 
+            largo = 40; 
+            peso = 25; 
+            valor = 150000;
+            break;
+        case "ventiladordepared":
+            alto = 55; 
+            ancho = 50; 
+            largo = 20; 
+            peso = 4; 
+            valor = 90000;
+            break;
+        case "colchon80cm":
+            alto = 20; 
+            ancho = 80; 
+            largo = 180; 
+            peso = 15; 
+            valor = 200000;
+            break;
+        case "colchon100cm":
+            alto = 20; 
+            ancho = 100; 
+            largo = 180; 
+            peso = 18; 
+            valor = 250000;
+            break;
+        case "colchon140cm":
+            alto = 20; 
+            ancho = 140; 
+            largo = 180; 
+            peso = 25; 
+            valor = 350000;
+            break;
+        case "colchon160cm":
+            alto = 20; 
+            ancho = 160; 
+            largo = 180; 
+            peso = 30; 
+            valor = 400000;
+            break;
+        case "colchon200cm":
+            alto = 20; 
+            ancho = 200; 
+            largo = 200; 
+            peso = 35; 
+            valor = 500000;
+            break;                        
         case "lavarropasCargaFrontal":
-            alto = 85; ancho = 60; largo = 60; peso = 70; valor = 800000;
+            alto = 85; 
+            ancho = 60; 
+            largo = 60; 
+            peso = 70; 
+            valor = 800000;
             break;
         case "lavarropasCargaSuperior":
-            alto = 100; ancho = 60; largo = 60; peso = 65; valor = 600000;
+            alto = 100; 
+            ancho = 60; 
+            largo = 60; 
+            peso = 65; 
+            valor = 600000;
             break;
         case "termotanque50":
-            alto = 60; ancho = 40; largo = 40; peso = 20; valor = 250000;
+            alto = 60; 
+            ancho = 40; 
+            largo = 40; 
+            peso = 20; 
+            valor = 250000;
             break;
         case "termotanque80":
-            alto = 80; ancho = 40; largo = 40; peso = 25; valor = 250000;
+            alto = 80; 
+            ancho = 40; 
+            largo = 40; 
+            peso = 25; 
+            valor = 250000;
             break;
         case "termotanque110":
-            alto = 100; ancho = 40; largo = 40; peso = 30; valor = 250000;
+            alto = 100; 
+            ancho = 40; 
+            largo = 40; 
+            peso = 30; 
+            valor = 250000;
             break;
         case "termotanque150":
-            alto = 150; ancho = 40; largo = 40; peso = 35; valor = 250000;
+            alto = 150; 
+            ancho = 40; 
+            largo = 40; 
+            peso = 35; 
+            valor = 250000;
             break;
         case "termotanque180":
-            alto = 180; ancho = 50; largo = 50; peso = 40; valor = 300000;
+            alto = 180; 
+            ancho = 50; 
+            largo = 50; 
+            peso = 40; 
+            valor = 300000;
             break;
         case "smartTV32":
-            alto = 45; ancho = 73; largo = 20; peso = 6; valor = 250000;
+            alto = 45; 
+            ancho = 73; 
+            largo = 20; 
+            peso = 6; 
+            valor = 250000;
             break;
         case "smartTV40":
-            alto = 55; ancho = 91; largo = 25; peso = 8; valor = 350000;
+            alto = 55; 
+            ancho = 91; 
+            largo = 25; 
+            peso = 8; 
+            valor = 350000;
             break;
         case "smartTV43":
-            alto = 58; ancho = 97; largo = 25; peso = 9; valor = 400000;
+            alto = 58; 
+            ancho = 97; 
+            largo = 25; 
+            peso = 9; 
+            valor = 400000;
             break;
         case "smartTV50":
-            alto = 65; ancho = 112; largo = 28; peso = 11; valor = 550000;
+            alto = 65; 
+            ancho = 112; 
+            largo = 28; 
+            peso = 11; 
+            valor = 550000;
             break;
         case "smartTV58":
-            alto = 73; ancho = 130; largo = 32; peso = 14; valor = 600000;
+            alto = 73; 
+            ancho = 130; 
+            largo = 32; 
+            peso = 14; 
+            valor = 600000;
             break;
         case "smartTV65":
-            alto = 81; ancho = 145; largo = 35; peso = 17; valor = 750000;
+            alto = 81; 
+            ancho = 145; 
+            largo = 35; 
+            peso = 17; 
+            valor = 750000;
             break;
         case "smartTV70":
-            alto = 90; ancho = 157; largo = 38; peso = 20; valor = 850000;
+            alto = 90; 
+            ancho = 157; 
+            largo = 38; 
+            peso = 20; 
+            valor = 850000;
             break;
         case "calefactor2000":
-            alto = 60; ancho = 70; largo = 30; peso = 15; valor = 150000;
+            alto = 60; 
+            ancho = 70; 
+            largo = 30; 
+            peso = 15; 
+            valor = 150000;
             break;
         case "calefactor3000":
-            alto = 70; ancho = 80; largo = 30; peso = 18; valor = 250000;
+            alto = 70; 
+            ancho = 80; 
+            largo = 30; 
+            peso = 18; 
+            valor = 250000;
             break;
         case "calefactor5000":
-            alto = 80; ancho = 100; largo = 30; peso = 22; valor = 350000;
+            alto = 80; 
+            ancho = 100; 
+            largo = 30; 
+            peso = 22; 
+            valor = 350000;
             break;
         case "calefactor8000":
-            alto = 90; ancho = 100; largo = 30; peso = 25; valor = 450000;
+            alto = 90; 
+            ancho = 100; 
+            largo = 30; 
+            peso = 25; 
+            valor = 450000;
             break;
         case "split2700":
-            alto = 49.5; ancho = 72; largo = 27; peso = 40; valor = 600000; // Medidas de la unidad exterior
+            alto = 49.5; 
+            ancho = 72; 
+            largo = 27; 
+            peso = 40; 
+            valor = 600000; // Medidas de la unidad exterior
             agregarBulto("split2700");
             break;
         case "split3300":
-            alto = 49.5; ancho = 72; largo = 27; peso = 50; valor = 700000; // Medidas de la unidad exterior
+            alto = 49.5; 
+            ancho = 72; 
+            largo = 27; 
+            peso = 50; 
+            valor = 700000; // Medidas de la unidad exterior
             agregarBulto("split3300");
             break;
         case "split4500":
-            alto = 30; ancho = 82; largo = 60.5; peso = 60; valor = 800000; // Medidas de la unidad exterior
+            alto = 30; 
+            ancho = 82; 
+            largo = 60.5; 
+            peso = 60; 
+            valor = 800000; // Medidas de la unidad exterior
             agregarBulto("split4500");
             break;
         case "split5500":
-            alto = 36; ancho = 90; largo = 38; peso = 80; valor = 900000; // Medidas de la unidad exterior
+            alto = 36; 
+            ancho = 90; 
+            largo = 38; 
+            peso = 80; 
+            valor = 900000; // Medidas de la unidad exterior
             agregarBulto("split5500");
             break;
         case "split6000":
-            alto = 110; ancho = 100.7; largo = 42.4; peso = 99; valor = 1100000; // Medidas de la unidad exterior
+            alto = 110; 
+            ancho = 100.7; 
+            largo = 42.4; 
+            peso = 99; 
+            valor = 1100000; // Medidas de la unidad exterior
             agregarBulto("split6000");
             break;
         case "pisoTecho18000":
-            alto = 139; ancho = 95; largo = 40; peso = 135; valor = 1500000; // Medidas de la unidad exterior
+            alto = 139; 
+            ancho = 95; 
+            largo = 40; 
+            peso = 135; 
+            valor = 1500000; // Medidas de la unidad exterior
             agregarBulto("pisoTecho18000");
             break;
         case "bulto20":
-            alto = 20; ancho = 20; largo = 20; peso = 1; valor = 10000;
+            alto = 20; 
+            ancho = 20; 
+            largo = 20; 
+            peso = 1; 
+            valor = 10000;
             break;
         case "bulto30":
-            alto = 30; ancho = 30; largo = 30; peso = 2; valor = 20000;
+            alto = 30; 
+            ancho = 30; 
+            largo = 30; 
+            peso = 2; 
+            valor = 20000;
             break;
         case "bulto40":
-            alto = 40; ancho = 40; largo = 40; peso = 3; valor = 30000;
+            alto = 40; 
+            ancho = 40; 
+            largo = 40; 
+            peso = 3; 
+            valor = 30000;
             break;
         case "bulto50":
-            alto = 50; ancho = 50; largo = 50; peso = 4; valor = 40000;
+            alto = 50; 
+            ancho = 50; 
+            largo = 50; 
+            peso = 4; 
+            valor = 40000;
             break;
         default:
             alto = ancho = largo = peso = valor = 0;
-    }
-
+    }    
     // Asignar medidas a los inputs de la unidad exterior
     document.getElementById("alto0").value = alto;
     document.getElementById("ancho0").value = ancho;
