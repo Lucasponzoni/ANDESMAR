d397ac5815bfe2ffb81392aa77025df1d0091f35
nombredeusuariolucasponzoni <139734105+Lucasponzoni@users.noreply.github.com>
2025-01-24 19:10:56 -0300

    Actualizar

--- js/formulario-cds.js
+++ js/formulario-cds.js
@@ -1,37 +1,110 @@
-endpoint: https://api-ventaenlinea.cruzdelsur.com/api/NuevaCotXVolEntregaYDespacho?idcliente=87231e4b-b414-47c0-882b-ef98adb94fe4&ulogin=necommerce&uclave=novogar71!&volumen=13500&peso=8&codigopostal=8400&localidad=Bariloche&valor=500&contrareembolso=&items=&despacharDesdeDestinoSiTieneAlmacenamiento=&queentrega=E&quevia=T&documento=33333333&nombre=Jose Perez&telefono=5555-5555&email=nada@nada.com&domicilio=San Martin 1522&bultos=2&referencia=9001-00012341&textosEtiquetasBultos&textoEtiquetaDocumentacion&devolverDatosParaEtiquetas=N
+async function enviarSolicitudCDS() {
+    console.log("Iniciando Generacion de Etiqueta CDS:"); // Para depuración
 
-idcliente 87231e4b-b414-47c0-882b-ef98adb94fe4
-ulogin necommerce
-uclave novogar71!
+    // Obtener valores de los elementos del DOM
+    const volumenTotalcds = parseFloat(document.getElementById("volumenTotalcm").innerText) || 0;
+    const pesoCds = document.getElementById("peso").value;
+    const codigoPostalCds = document.getElementById("codigoPostalDestinatario").value;
+    const localidadCds = document.getElementById('localidad').value;
+    const valorCds = document.getElementById("valorDeclarado").value;
+    const queEntregaCds = document.getElementById("tipoEntregaCruzDelSur").value;
+    const documentoCds = document.getElementById('nroRemito').value;
+    const nombreCds = document.getElementById('nombreApellidoDestinatario').value;
+    const telefonoCds = document.getElementById("telefonoDestinatario").value;
+    const emailCds = document.getElementById('emailDestinatario').value;
+    const domicilioCds = document.getElementById('calleDestinatario').value + ' ' + document.getElementById('calleNroDestinatario').value;
 
-volumen <span id="volumenTotalcm">0.00</span>
-peso document.getElementById("peso").value;
-codigopostal document.getElementById("calleDestinatario").value;
-localidad document.getElementById('localidad').value,
-valor document.getElementById("valorDeclarado").value;
-contrareembolso ""
-items ""
-despacharDesdeDestinoSiTieneAlmacenamiento ""
-queentrega "E" Tipo de entrega a cotizar .'E' para entrega a domicilio y 'R'
-quevia "T"
-documento "68543701"
-nombre document.getElementById("codigoPostalDestinatario").value;
-telefono document.getElementById("telefonoDestinatario").value;
-email document.getElementById('emailDestinatario').value,
-domicilio document.getElementById('calleDestinatario').value, + document.getElementById('calleNroDestinatario').value,
-bultos     bultoElements.forEach(bulto => {
-            const cantidadBULTO = parseInt(bulto.querySelector(`input[name^="Cantidad"]`).value) || 1; // Obtener cantidad
-referencia document.getElementById("nroRemito").value;
-textosEtiquetasBultos ""
-devolverDatosParaEtiquetas N
+    let totalBultosCds = 0;
+    const bultoElements = document.querySelectorAll('.bulto');
 
-Response: 
-{"Respuesta":[{"Estado":0,"Descripcion":"","NumeroCotizacion":17552621,"NIC":"166543607","UrlSeguimiento":"https://www.cruzdelsur.com/herramientas_seguimiento_resultado.php?nic=166543607"}],"Cotizaciones":[{"CodigoLinea":"17552621-1-2-T","Valor":16120.13,"ValorTxt":"$ 16120,13","HorasDesde":48,"HorasHasta":120,"HorasDesdeTxt":"48 Hs.","HorasHastaTxt":"120 Hs.","HorasDesdeHastaTxt":"48 / 120 Hs.","Descripcion":"Entrega en domicilio","TipoDeEntrega":"E","Via":"T","DescripcionLarga":"","DescripcionParaWeb":"","ValorAdicionalRetiro":0.0,"ValorAdicionalRetiroTxt":"$ 0,00","ValorAdicionalReceptoria":0.0,"ValorAdicionalReceptoriaTxt":"$ 0,00","PorcentajeDeIva":21.0,"PorcentajeDeIvaTxt":"21,00 %","ValoresIncluyenIva":"NO"}],"Sucursal":[{"IdSucursal":408,"Nombre":"Bariloche","Ciudad":"Bariloche","Provincia":"Río Negro","Domicilio":"Vereertbrügghen 2030 (ex 1730)","CP":"8403","Telefono":"(0294) 442 2490 / 1771","Email":"info-bariloche@cruzdelsur.com","Horario":"L/V 8:00 a 18:00hs. - S 9:00 a 13:00 hs.","Longitud":-71.281,"Latitud":-41.129}]}
+    bultoElements.forEach(bulto => {
+        const cantidadCds = parseInt(bulto.querySelector(`input[name^="Cantidad"]`).value) || 1; 
+        totalBultosCds += cantidadCds;
+    });
 
-Si se duplica remito:
-{"Respuesta":[{"Estado":25,"Descripcion":"Ya existe un despacho con el mismo numero de referencia.","NumeroCotizacion":17552092,"NIC":"166539907","UrlSeguimiento":""}]}
+    const referenciaCds = document.getElementById("nroRemito").value;
 
+    document.getElementById("descargaCruzDelSur").style.display = 'block'; // Mostrar la sección de descarga
 
-Para boton de etiqueta:
+    const urlCds = `https://proxy.cors.sh/https://api-ventaenlinea.cruzdelsur.com/api/NuevaCotXVolEntregaYDespacho?idcliente=87231e4b-b414-47c0-882b-ef98adb94fe4&ulogin=necommerce&uclave=novogar71!&volumen=${volumenTotalcds}&peso=${pesoCds}&codigopostal=${codigoPostalCds}&localidad=${localidadCds}&valor=${valorCds}&contrareembolso=&items=&despacharDesdeDestinoSiTieneAlmacenamiento=&queentrega=${queEntregaCds}&quevia=T&documento=${documentoCds}&nombre=${nombreCds}&telefono=${telefonoCds}&email=${emailCds}&domicilio=${domicilioCds}&bultos=${totalBultosCds}&referencia=${referenciaCds}&textosEtiquetasBultos&textoEtiquetaDocumentacion&devolverDatosParaEtiquetas=N`;
 
-endpoint: https://api-ventaenlinea.cruzdelsur.com/api/EtiquetasPDF?idcliente=87231e4b-b414-47c0-882b-ef98adb94fe4&ulogin=necommerce&uclave=novogar71!&id=17549793&tamanioHoja=1&posicionArrancar=1&textoEspecialPorEtiqueta=
+    const optionsCds = {
+        method: 'GET',
+        headers: {
+            'x-cors-api-key': 'live_36d58f4c13cb7d838833506e8f6450623bf2605859ac089fa008cfeddd29d8dd'
+        }
+    };
+
+    try {
+        // Mostrar el spinner
+        document.getElementById("apiResponseCruzDelSur").innerHTML = `
+            <button class="btn btn-dark-blue" type="button" disabled>
+                <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+                Cargando Etiqueta...
+            </button>
+        `;
+
+        const responseCds = await fetch(urlCds, optionsCds);
+        const dataCds = await responseCds.json();
+        console.log(dataCds); // Para depuración
+
+        // Manejo de la respuesta
+        if (dataCds.Respuesta[0].Estado === 0) {
+            const numeroCotizacionCds = dataCds.Respuesta[0].NumeroCotizacion;
+            const nicCds = dataCds.Respuesta[0].NIC;
+
+            // Actualizar el título y el botón
+            document.getElementById("numeroDeEnvioCruzDelSur").innerText = `NIC-${nicCds}`;
+            document.getElementById("titleCruzDelSur").innerHTML = `
+                <span><img class="surprise" src="./Img/download-file.gif"> CRUZ DEL SUR NIC-${nicCds}<span id="numeroDeEnvioCruzDelSur"></span></span>
+            `;
+            document.getElementById("nombreCruzDelSur").innerText = nombreCds;
+            document.getElementById("nombreCruzDelSur").style.display = 'none'; // Mostrar el nombre
+
+            // Llamar a la API para descargar la etiqueta
+            await descargarEtiqueta(numeroCotizacionCds);
+        } else if (dataCds.Respuesta[0].Estado === 25) {
+            // Manejo de duplicados
+            const duplicadoNicCds = dataCds.Respuesta[0].NIC;
+            document.getElementById("numeroDeEnvioCruzDelSur").innerText = duplicadoNicCds;
+            document.getElementById("titleCruzDelSur").innerHTML = `
+                <img class="surprise" src="./Img/404.gif"> CRUZ DEL SUR - DUPLICA REMITO EN SERVIDOR
+            `;
+            document.getElementById("apiResponseCruzDelSur").innerHTML = `
+                <button class="btn btn-dark-blue" type="button">
+                <i class="bi bi-exclamation-triangle-fill" style="margin-right: 8px;"></i> El Remito ya fue utilizado en NIC-${duplicadoNicCds}
+                </button>
+            `;
+        }
+    } catch (error) {
+        console.error("Error al crear la cotización:", error);
+        document.getElementById("errorResponseCruzDelSur").innerText = "Ocurrió un error al crear la cotización. Por favor, intenta nuevamente.";
+    }
+}
+
+async function descargarEtiqueta(numeroCotizacionCds) {
+    const urlEtiquetaCds = `https://proxy.cors.sh/https://api-ventaenlinea.cruzdelsur.com/api/EtiquetasPDF?idcliente=87231e4b-b414-47c0-882b-ef98adb94fe4&ulogin=necommerce&uclave=novogar71!&id=${numeroCotizacionCds}&tamanioHoja=1&posicionArrancar=1&textoEspecialPorEtiqueta=`;
+
+    const optionsEtiquetaCds = {
+        method: 'GET',
+        headers: {
+            'x-cors-api-key': 'live_36d58f4c13cb7d838833506e8f6450623bf2605859ac089fa008cfeddd29d8dd'
+        }
+    };
+
+    try {
+        const responseEtiquetaCds = await fetch(urlEtiquetaCds, optionsEtiquetaCds);
+        const blobCds = await responseEtiquetaCds.blob();
+        const urlCds = window.URL.createObjectURL(blobCds);
+
+        // Actualizar el botón para descargar el PDF
+        document.getElementById("apiResponseCruzDelSur").innerHTML = `
+            <a class="btn btn-dark-blue" href="${urlCds}" target="_blank">
+                <i class="bi bi-filetype-pdf" style="margin-right: 8px;"></i> Descargar Etiqueta PDF
+            </a>
+        `;
+    } catch (error) {
+        console.error("Error al descargar la etiqueta:", error);
+        document.getElementById("errorResponseCruzDelSur").innerText = "Ocurrió un error al descargar la etiqueta. Por favor, intenta nuevamente.";
+    }
+}
