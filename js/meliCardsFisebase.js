45c7d4b0aa9d08b2c4122a627a0e59ae3349ab15
LUCAS PONZONI <139734105+Lucasponzoni@users.noreply.github.com>
2025-02-28 12:31:03 -0300

    Actualizar

--- js/meliCardsFisebase.js
+++ js/meliCardsFisebase.js
@@ -2479,10 +2479,10 @@ async function generarPDF(email, id, NombreyApellido, Cp, idOperacion, calleDest
         const diasDeLaSemana = {
             'lunes': 1,
             'martes': 2,
-            'miércoles': 3,
+            'miercoles': 3,
             'jueves': 4,
             'viernes': 5,
-            'sábado': 6,
+            'sabado': 6,
             'domingo': 0
         };
         
