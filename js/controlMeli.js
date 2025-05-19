145fc8fa7408b22a08ae826e44305250a5df88b4
LUCAS PONZONI <139734105+Lucasponzoni@users.noreply.github.com>
2025-05-19 18:41:10 -0300

    Actualizar

--- js/controlMeli.js
+++ js/controlMeli.js
@@ -1,12 +1,13 @@
 // Configuración de Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyCMu2vPvNzhv0cM3b4RItmqZybRhhR_HJM",
-    authDomain: "despachos-meli-novogar.firebaseapp.com",
-    projectId: "despachos-meli-novogar",
-    storageBucket: "despachos-meli-novogar.appspot.com",
-    messagingSenderId: "774252628334",
-    appId: "1:774252628334:web:623aa84bc3b1cebd3f997f",
-    measurementId: "G-E0E9K4TEDW"
+  apiKey: "AIzaSyCMu2vPvNzhv0cM3b4RItmqZybRhhR_HJM",
+  authDomain: "despachos-meli-novogar.firebaseapp.com",
+  databaseURL: "https://despachos-meli-novogar-default-rtdb.firebaseio.com",
+  projectId: "despachos-meli-novogar",
+  storageBucket: "despachos-meli-novogar.appspot.com",
+  messagingSenderId: "774252628334",
+  appId: "1:774252628334:web:623aa84bc3b1cebd3f997f",
+  measurementId: "G-E0E9K4TEDW"
 };
 
 // Inicializar Firebase
